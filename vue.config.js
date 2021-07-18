74d22ca9860551331742832496730a383531aba0
ihgSherryLee <lxyihggyml@163.com>
2021-07-18 12:40:48 +0800

    eladmin

--- vue.config.js
+++ vue.config.js
@@ -35,8 +35,8 @@ module.exports = {
     overlay: {
       warnings: false,
       errors: true
-    },
-    before: require('./mock/mock-server.js')
+    }
+    // before: require('./mock/mock-server.js')
   },
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
@@ -44,7 +44,8 @@ module.exports = {
     name: name,
     resolve: {
       alias: {
-        '@': resolve('src')
+        '@': resolve('src'),
+        '@crud': resolve('src/components/Crud')
       }
     }
   },
